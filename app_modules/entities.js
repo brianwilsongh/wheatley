8b2ac9b18769216c78ee5cd0b62f980b025abdc6
wilsontheory <wilson.brian90@gmail.com>
2018-01-21 21:59:52 +0700

    Add missing module.exports

--- app_modules/entities.js
+++ app_modules/entities.js
@@ -1,16 +1,24 @@
-function processGreetings(items){
-  for (var idx = 0; idx < items.length; idx++){
-    var item = items[idx];
-    if (item.confidence >= 0.85) { 
-      return {text: "Why hello there!"};
-    } else {
-      return {text: "...huh?"};
+module.exports = {
+  function processGreetings(items) {
+    for (var idx = 0; idx < items.length; idx++) {
+      var item = items[idx];
+      if (item.confidence >= 0.85) {
+        return {
+          text: "Why hello there!"
+        };
+      } else {
+        return {
+          text: "...huh?"
+        };
+      }
     }
   }
-}
 
-function processDateTimes(items){
-  for (let item in items){
-    if (item.confidence > 0.80) return {text: item.value + " is the date I got"};
+  function processDateTimes(items) {
+    for (let item in items) {
+      if (item.confidence > 0.80) return {
+        text: item.value + " is the date I got"
+      };
+    }
   }
 }
